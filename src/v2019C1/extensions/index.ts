6ccbb72d8720cbd73c8ba1ebc3f041bfafcec470
Michel Guerin <dev@guerin.at>
2026-04-21 09:37:34 +0200

    fix(extensions): add cleanUp to the public api

--- src/v2019C1/extensions/index.ts
+++ src/v2019C1/extensions/index.ts
@@ -1,6 +1,7 @@
+import { cleanUp } from './clean-up'
 import { dataModel } from './data-model'
 import { history } from './history'
 import { reference } from './reference'
 import { template } from './template'
 
-export const SCL_EXTENSION_MODULES = { history, dataModel, reference, template }
+export const SCL_EXTENSION_MODULES = { cleanUp, dataModel, history, reference, template }
