2ea7a2ab5545996c8bd605592a180ab28e3da8e9
Michel Guerin <dev@guerin.at>
2026-04-30 18:04:12 +0200

    fix(extensions): expose presentation

--- src/v2019C1/extensions/index.ts
+++ src/v2019C1/extensions/index.ts
@@ -1,7 +1,15 @@
 import { cleanUp } from './clean-up'
 import { dataModel } from './data-model'
 import { history } from './history'
+import { presentation } from './presentation'
 import { reference } from './reference'
 import { template } from './template'
 
-export const SCL_EXTENSION_MODULES = { cleanUp, dataModel, history, reference, template }
+export const SCL_EXTENSION_MODULES = {
+	cleanUp,
+	dataModel,
+	history,
+	reference,
+	template,
+	presentation,
+}
