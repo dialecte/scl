a61e175c20ddc5c9439255fbdad18ae190fa3099
Michel Guerin <dev@guerin.at>
2026-05-19 13:52:51 +0200

    feat(scl): update core to latest version

--- src/v2019C1/test/hydrated-test.ts
+++ src/v2019C1/test/hydrated-test.ts
@@ -8,24 +8,34 @@ import {
 } from '@dialecte/core/test'
 
 import { SCL_DIALECTE_CONFIG } from '@/v2019C1/config'
+import { SCL_EXTENSION_MODULES } from '@/v2019C1/extensions'
 import { HOOKS } from '@/v2019C1/hooks'
 
 import type { Config } from '@/v2019C1/config/dialecte.config'
+
+type SclModules = typeof SCL_EXTENSION_MODULES
 
 export const XMLNS_SCL_NAMESPACE = `xmlns="http://www.iec.ch/61850/2003/SCL"`
 export const XMLNS_SCL_6_100_NAMESPACE = `xmlns:eIEC61850-6-100="http://www.iec.ch/61850/2019/SCL/6-100"`
 export const ALL_XMLNS_NAMESPACES = `${XMLNS_SCL_NAMESPACE} ${XMLNS_SCL_6_100_NAMESPACE} ${XMLNS_DEV_NAMESPACE}`
 export { CUSTOM_RECORD_ID_ATTRIBUTE, CUSTOM_RECORD_ID_ATTRIBUTE_NAME }
 
-export const runSclTestCases = createTestRunner(SCL_DIALECTE_CONFIG, HOOKS)
+const SCL_EXTENSIONS = { base: SCL_EXTENSION_MODULES }
+
+export const runSclTestCases = createTestRunner<Config, SclModules>({
+	dialecteConfig: SCL_DIALECTE_CONFIG,
+	hooks: HOOKS,
+	extensions: SCL_EXTENSIONS,
+})
 
 export async function createSclTestProject(params: { sourceXml: string; targetXml?: string }) {
 	const { sourceXml, targetXml } = params
 
-	return createTestProject({
+	return createTestProject<Config, SclModules>({
 		sourceXml,
 		targetXml,
 		dialecteConfig: SCL_DIALECTE_CONFIG,
+		extensions: SCL_EXTENSIONS,
 		hooks: HOOKS,
 	})
 }
